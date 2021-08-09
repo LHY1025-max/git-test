6e53bc7156bc74abd342bf27884e7411f2ad46d5
luohaoyi <534131274@qq.com>
2021-08-09 21:39:18 +0800

    push commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("Hello hot-fix");
         System.out.println("111111111111111");
         System.out.println("222222222222");
+        System.out.println("push test");
     }
 }
