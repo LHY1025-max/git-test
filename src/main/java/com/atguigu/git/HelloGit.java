bf3df729301b184c6e3cea18f5fb38c9ee9a8d5c
luohaoyi <534131274@qq.com>
2021-08-09 22:56:51 +0800

    Revert "push commit"
    This reverts commit 6e53bc71

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -2,7 +2,13 @@ package com.atguigu.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-       
-        System.out.println("pull test");
+        System.out.println("Hello Git!!!!");
+        System.out.println("Hello Git!!!!");
+        System.out.println("Hello Git!!!!");
+        System.out.println("Hello Git33!!!!");
+        System.out.println("Hello Git33!!!!");
+        System.out.println("Hello hot-fix");
+        System.out.println("111111111111111");
+        System.out.println("222222222222");
     }
 }
