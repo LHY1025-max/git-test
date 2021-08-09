8525fb450d808178465f0177fabf8aaf9a5798a2
LHY1025-max <78318431+LHY1025-max@users.noreply.github.com>
2021-08-09 22:00:38 +0800

    pull commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("111111111111111");
         System.out.println("222222222222");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
